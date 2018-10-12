32403060031780445ab8e9bc3573fb5593a656b1
Antonio Proenca <antproenca92@gmail.com>
2018-10-12 19:34:58 -0300

    Adição de paginação as categorias, validação sintatica com beanValidation

--- src/main/java/com/proenca/services/CategoriaService.java
+++ src/main/java/com/proenca/services/CategoriaService.java
@@ -5,8 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.proenca.DTO.CategoriaDTO;
 import com.proenca.domain.Categoria;
 import com.proenca.repositories.CategoriaRepository;
 import com.proenca.service.exceptions.DataIntegrityException;
@@ -14,26 +18,26 @@ import com.proenca.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-			"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
-		return repo.save(obj);		
+		return repo.save(obj);
 	}
 
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
 	}
- 
+
 	public void delete(Integer id) {
 		find(id);
 		try {
@@ -45,6 +49,15 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
