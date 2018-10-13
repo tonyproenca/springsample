e214c8c65bc5ddd347af9fb228ebdfd5dde3fa85
Antonio Proenca <antproenca92@gmail.com>
2018-10-12 23:16:04 -0300

    Validação para evitar emails duplicados

--- src/main/java/com/proenca/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/proenca/services/validation/ClienteInsertValidator.java
@@ -5,12 +5,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.proenca.DTO.ClienteNewDTO;
+import com.proenca.domain.Cliente;
 import com.proenca.domain.enums.TipoCliente;
+import com.proenca.repositories.ClienteRepository;
 import com.proenca.resources.Exceptions.FieldMessage;
 import com.proenca.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -18,13 +26,26 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDTO, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
+		/*
+		 * Validação se o numero inserido para CPF é válido
+		 */
 		if (objDTO.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCpf(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
 		}
 		
+		/*
+		 * Validação se o numero inserido para CNPJ é valido
+		 */
 		if (objDTO.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCpf(objDTO.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		/*
+		 * Validação se o email já existe na base de dados
+		 */
+		Cliente aux = repo.findByEmail(objDTO.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		
 		for (FieldMessage e : list) {
