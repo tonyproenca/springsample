641edf5024b9bea05e61cff66199110089652d07
Antonio Proença <antproenca92@gmail.com>
2018-10-14 12:21:17 -0300

    criação do profile de desenvolvimento adição da dependencia javassist, para possibilitar conexão com base de dados utilizando properties diversas

---
+++ src/main/java/com/proenca/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.proenca.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.proenca.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+	
+	@Autowired 
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.InstantiateTestDatabase();
+		return true;
+	}
+
+}
